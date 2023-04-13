c99611e2c9f6aef890fc51d19fce81856d2ced1e
antonio.virgone <antonio.virgone@wisemotions.com>
2023-04-13 16:15:29 +0200

    Initial Commit

---
+++ personalTrainer/personalTrainerApp.swift
@@ -0,0 +1,17 @@
+//
+//  personalTrainerApp.swift
+//  personalTrainer
+//
+//  Created by Antonio Virgone on 13/04/23.
+//
+
+import SwiftUI
+
+@main
+struct personalTrainerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
